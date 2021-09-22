fb1a535a068973a685c8c5e519500effcba0804e
Merlina <89131331+M-e-r-l-i-n-a@users.noreply.github.com>
2021-09-22 08:47:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LA_1200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Geheimzahl = New Random(1, 100);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
